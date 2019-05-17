c232f31ef4d5a1d10c8c930ed7cca66d24592342
Eugio <47667122+ejhalpin@users.noreply.github.com>
2019-05-17 13:13:22 -0400

    added UI Keyboard with letters and game-level functionality; display is fine on mobile devices -- some scrolling may be required

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+document.onkeydown = function() {
+  getKeyPressed(event);
+};
 //global variables
 var targetLetter;
 var wins = 0;
@@ -5,9 +8,9 @@ var losses = 0;
 var guessCount = 10;
 var guesses = "";
 var boot = false;
-let letters = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const messages = [
-  "press any key to begin...",
+  'press any key to play. <i id="keyboard-icon" class="fas fa-keyboard" onclick="doLayout()">',
   "Hmmm... Let me think...",
   "I've got my letter. Start guessing.",
   "Nope. Guess again!",
@@ -29,15 +32,15 @@ function bootup() {
 }
 
 function update() {
-  document.getElementById("wins").value = wins.toString();
-  document.getElementById("losses").value = losses.toString();
-  document.getElementById("guess-remain").value = guessCount.toString();
-  document.getElementById("guesses").value = guesses;
+  document.getElementById("wins").textContent = wins.toString();
+  document.getElementById("losses").textContent = losses.toString();
+  document.getElementById("guesses-remaining").textContent = guessCount.toString();
+  document.getElementById("guesses").textContent = guesses;
 }
 
 function start() {
-  document.getElementById("letterbox").textContent = "";
-  document.getElementById("letterbox").textContent = "?";
+  document.getElementById("letter-box").textContent = "";
+  document.getElementById("letter-box").textContent = "?";
   targetLetter = letters[Math.floor(Math.random() * 25)];
   guessCount = 10;
   guesses = "";
@@ -46,7 +49,7 @@ function start() {
 
 function getKeyPressed(event) {
   var key = event.key;
-  key = key.toLowerCase();
+  console.log(key);
   if (!boot) {
     bootup();
     return;
@@ -54,11 +57,11 @@ function getKeyPressed(event) {
   if (key === targetLetter) {
     document.getElementById("message").textContent = messages[6];
     wins++;
-    document.getElementById("letterbox").textContent = "";
-    document.getElementById("letterbox").textContent = targetLetter;
+    document.getElementById("letter-box").textContent = "";
+    document.getElementById("letter-box").textContent = targetLetter;
     update();
     window.setTimeout(function() {
-      document.getElementById("message").textContent = messages[0];
+      document.getElementById("message").innerHTML = messages[0];
       boot = false;
     }, 1500);
     return;
@@ -80,9 +83,8 @@ function getKeyPressed(event) {
     losses++;
     update();
     window.setTimeout(function() {
-      document.getElementById("message").textContent = messages[0];
-      document.getElementById("letterbox").textContent = "";
-      document.getElementById("letterbox").textContent = targetLetter;
+      document.getElementById("message").innerHTML = messages[0];
+      document.getElementById("letter-box").textContent = targetLetter;
       boot = false;
     }, 1500);
     return;
@@ -97,6 +99,54 @@ function getKeyPressed(event) {
   }
 }
 
-function giveFocus() {
-  document.getElementById("autokey").focus();
+// document.getElementById("keyboard-icon").ontouchend = function() {
+//   doLayout();
+// };
+
+var keyboard_display = false;
+let keys = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+//numeric keyboard codes for letter range from a=65 to z=90. to get the codes, simply querry the index of the letter
+//in the letters array and add 65.
+var keyboard = document.getElementById("keyboard");
+var notepad = document.getElementById("notepad");
+
+function doLayout() {
+  if (document.getElementById("keyboard").children.length == 0) {
+    for (var i = 0; i < keys.length; i++) {
+      var key = document.createElement("div");
+      key.id = keys[i];
+      key.style.gridArea = keys[i];
+      key.textContent = keys[i];
+      key.className = "key";
+      key.onclick = function() {
+        var event_name = "keydown";
+        var event = document.createEvent("HTMLEvents");
+        event.initEvent(event_name, true, false);
+        event.key = this.id;
+        document.dispatchEvent(event);
+      };
+      keyboard.appendChild(key);
+    }
+    return;
+  } else {
+    //var keyboard = document.getElementById("keyboard");
+    for (var i = 0; i < keys.length; i++) {
+      var key = document.getElementById([keys[i]]);
+      keyboard.removeChild(key);
+    }
+  }
+}
+
+function printKey(letter) {
+  var l = letter;
+  // if (uppercase) {
+  //   l = letter.toUpperCase();
+  // }
+  var text = notepad.textContent;
+  if (l.length === 0) {
+    text = text.substring(0, text.length - 1);
+  } else {
+    text = text + l;
+  }
+  notepad.textContent = text;
 }
